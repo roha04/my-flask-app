3f887e8bbe34b0ccf052de4181ca0634515b5711
csv12cr0 <csv12cr0@cloud-mentor.local>
2026-03-18 09:31:55 +0000

    Update application.py for verification

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "Hello from csv12cr0 Elastic Beanstalk CI-CD"
+    return "Hello from csv12cr0 Elastic Beanstalk CI-CD verification-2026031809035521200"
 
 if __name__ == "__main__":
     application.run()
